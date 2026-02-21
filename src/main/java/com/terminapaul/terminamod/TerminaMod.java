c314090e96916b8014d6405e920f26c65e2b8166
Paul <paul.ull.leathers@gmail.com>
2026-02-21 18:39:02 +0100

    1.0.0

--- src/main/java/com/terminapaul/terminamod/TerminaMod.java
+++ src/main/java/com/terminapaul/terminamod/TerminaMod.java
@@ -5,6 +5,7 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 @Mod(TerminaMod.MOD_ID)
@@ -13,7 +14,10 @@ public class TerminaMod {
     public static final String MOD_ID = "terminamod";
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public TerminaMod(IEventBus modEventBus) {
+    @SuppressWarnings("'get()' is deprecated since version 1.21.1 and marked for removal")
+    public TerminaMod() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
         ModBlocks.register(modEventBus);
         ModItems.register(modEventBus);
         ModCreativeModeTabs.register(modEventBus);
