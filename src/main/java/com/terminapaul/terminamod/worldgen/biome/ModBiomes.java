2884a3272663a5cfa8a059001e90e6477238f683
Paul <paul.ull.leathers@gmail.com>
2026-02-23 19:55:47 +0100

    1.1.2 (test fonctionel !!!!!!)

--- src/main/java/com/terminapaul/terminamod/worldgen/biome/ModBiomes.java
+++ src/main/java/com/terminapaul/terminamod/worldgen/biome/ModBiomes.java
@@ -2,13 +2,12 @@ package com.terminapaul.terminamod.worldgen.biome;
 
 import com.terminapaul.terminamod.TerminaMod;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.biome.*;
-import net.minecraft.data.worldgen.BiomeDefaultFeatures;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
-import net.minecraftforge.eventbus.api.IEventBus;
 
 public class ModBiomes {
     public static final DeferredRegister<Biome> BIOMES =
@@ -23,21 +22,33 @@ public class ModBiomes {
 
     public static Biome rubyHighlands() {
         MobSpawnSettings.Builder spawnBuilder = new MobSpawnSettings.Builder();
-        BiomeDefaultFeatures.commonSpawns(spawnBuilder);
+        spawnBuilder.addSpawn(MobCategory.CREATURE,
+                new MobSpawnSettings.SpawnerData(EntityType.SHEEP, 12, 4, 4));
+        spawnBuilder.addSpawn(MobCategory.CREATURE,
+                new MobSpawnSettings.SpawnerData(EntityType.COW, 10, 4, 4));
+        spawnBuilder.addSpawn(MobCategory.CREATURE,
+                new MobSpawnSettings.SpawnerData(EntityType.PIG, 10, 4, 4));
+        spawnBuilder.addSpawn(MobCategory.MONSTER,
+                new MobSpawnSettings.SpawnerData(EntityType.ZOMBIE, 95, 4, 4));
+        spawnBuilder.addSpawn(MobCategory.MONSTER,
+                new MobSpawnSettings.SpawnerData(EntityType.SKELETON, 100, 4, 4));
+        spawnBuilder.addSpawn(MobCategory.MONSTER,
+                new MobSpawnSettings.SpawnerData(EntityType.CREEPER, 100, 4, 4));
 
+        // Les features (minerais, arbres, etc.) sont définies dans ruby_highlands.json
         BiomeGenerationSettings.PlainBuilder genBuilder = new BiomeGenerationSettings.PlainBuilder();
 
         return new Biome.BiomeBuilder()
                 .hasPrecipitation(true)
-                .temperature(0.3f)
+                .temperature(0.25f)
                 .downfall(0.4f)
                 .specialEffects(new BiomeSpecialEffects.Builder()
-                        .skyColor(0x78A7FF)
+                        .skyColor(0x84BFFF)
                         .fogColor(0xC0D8FF)
                         .waterColor(0x3F76E4)
                         .waterFogColor(0x050533)
-                        .grassColorOverride(0x7A0000)
-                        .foliageColorOverride(0x7A0000)
+                        .grassColorOverride(0x5DEB3A)   // vert pomme vif
+                        .foliageColorOverride(0x4AC41E)
                         .build())
                 .mobSpawnSettings(spawnBuilder.build())
                 .generationSettings(genBuilder.build())
