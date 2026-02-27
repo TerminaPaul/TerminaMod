fc7dd0f3a722ece758f2f4ed17bc07cc98a036f4
Paul <paul.ull.leathers@gmail.com>
2026-02-27 23:00:34 +0100

    1.1.4 Hammer fix

--- src/main/java/com/terminapaul/terminamod/ObsidianHammerItem.java
+++ src/main/java/com/terminapaul/terminamod/ObsidianHammerItem.java
@@ -3,6 +3,7 @@ package com.terminapaul.terminamod;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.enchantment.Enchantments;
 
 public class ObsidianHammerItem extends Item {
@@ -11,10 +12,25 @@ public class ObsidianHammerItem extends Item {
         super(properties);
     }
 
-    @Override
-    public boolean canApplyAtEnchantingTable(ItemStack stack, Enchantment enchantment) {
+    private boolean isAllowed(Enchantment enchantment) {
         return enchantment == Enchantments.UNBREAKING
                 || enchantment == Enchantments.MENDING
                 || enchantment == Enchantments.BLOCK_FORTUNE;
+    }
+
+    @Override
+    public int getEnchantmentValue() {
+        return 10;
+    }
+
+    @Override
+    public boolean canApplyAtEnchantingTable(ItemStack stack, Enchantment enchantment) {
+        return isAllowed(enchantment);
+    }
+
+    @Override
+    public boolean isBookEnchantable(ItemStack stack, ItemStack book) {
+        return EnchantmentHelper.getEnchantments(book).keySet().stream()
+                .allMatch(this::isAllowed);
     }
 }
